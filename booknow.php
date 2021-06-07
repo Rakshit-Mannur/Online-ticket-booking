b57ec10916914bc398042afcf8c2c08be99f36ce
Rakshit Mannur <rakshitmannur00@gmail.com>
2021-06-08 00:06:03 +0530

    responsive

--- booknow.php
+++ booknow.php
@@ -33,12 +33,12 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#routes">Routes</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#status">Status</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/starks/home/home.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/starks/home/home.html#routes">Routes</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/starks/services/index.html">Services</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/starks/status/index.html">Status</a></li>
                     <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#login">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/starks/login.php">Login</a></li>
                 </ul>
             </div>
         </div>
